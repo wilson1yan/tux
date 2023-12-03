e449f081cde255f0d8fd71161dd4ecd091ae150c
Wilson Yan <wilson1.yan@berkeley.edu>
2023-12-02 17:23:43 -0800

    faster:

--- tux/checkpoint.py
+++ tux/checkpoint.py
@@ -1,4 +1,5 @@
 import os
+from concurrent.futures import ThreadPoolExecutor
 
 import flax
 import jax
@@ -50,14 +51,36 @@ class StreamingCheckpointer(object):
         train_state = to_state_dict(train_state)
         packer = msgpack.Packer()
         flattend_train_state = flatten_dict(train_state)
-        if gather_fns is not None:
-            gather_fns = flatten_dict(to_state_dict(gather_fns))
 
+        if gather_fns is None:
+            with open_file(path, "wb") as fout:
+                for key, value in flattend_train_state.items():
+                    value = float_tensor_to_dtype(value, float_dtype)
+                    fout.write(packer.pack((key, to_bytes(value))))
+            return
+        
         from tqdm import tqdm
+        gather_fns = flatten_dict(to_state_dict(gather_fns))
+
+        print('Lowering')
+        lowered_fns = dict()
+        for key, value in tqdm(flattend_train_state.items(), total=len(flattend_train_state)):
+            lowered_fns[key] = gather_fns[key][0](value)
+        
+        print('Compiling')
+        compiled_fns = dict()
+        with ThreadPoolExecutor() as executor:
+            for key in flattend_train_state.keys():
+                lowered_fn = lowered_fns[key]
+                value = flattend_train_state[key]
+                compiled_fns[key] = executor.submit(lowered_fn.compile)
+            for key in tqdm(flattend_train_state.keys(), totla=len(flattend_train_state)):
+                compiled_fns[key] = compiled_fns[key].result() 
+
+        print('Saving')
         with open_file(path, "wb") as fout:
             for key, value in tqdm(flattend_train_state.items(), total=len(flattend_train_state)):
-                if gather_fns is not None:
-                    value = gather_fns[key](value)
+                value = compiled_fns[key](value)
                 value = float_tensor_to_dtype(value, float_dtype)
                 fout.write(packer.pack((key, to_bytes(value))))
 
